082b6ebfafcf67d32739afabb6a9008b8bab6584
ykumar-rb <yogesh.kumar@riverbed.com>
2018-05-31 22:24:05 +0530

    create ui changes

--- handlers/ninja_web.go
+++ handlers/ninja_web.go
@@ -4,7 +4,9 @@ import (
 	"github.com/CloudInstall/libhttp"
 	"html/template"
 	"net/http"
-        "fmt"
+	"fmt"
+	"os"
+	"io"
 )
 
 func GetCreate(w http.ResponseWriter, r *http.Request) {
@@ -41,18 +43,47 @@ func ProcessCreate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	tmpl.Execute(w, "")
-	PrintSubmissionReport(w,r)
-//	r.ParseForm()
-//	fmt.Fprintln(w, r.Form)
+	//PrintSubmissionReport(w,r)
+
+
+	r.ParseMultipartForm(32 << 20)
+	file, handler, err := r.FormFile("uploadfile")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	fmt.Fprintf(w, "%v", handler.Header)
+	filePath := "./ZTPFiles/"+handler.Filename
+	f, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	io.Copy(f, file)
+
+
+	UploadedFileName := handler.Filename
+	fmt.Printf("Uploaded file:%s filePath:%s", UploadedFileName, filePath)
+
+	r.ParseForm()
+	fmt.Fprintln(w, r.Form)
+	fmt.Printf("********************")
+	fmt.Printf(r.Form.Get("maclist"))
 
 }
 
-func PrintSubmissionReport(w http.ResponseWriter,r *http.Request){
+func PrintSubmissionReport(w http.ResponseWriter,r *http.Request) {
 	r.ParseForm()
 	fmt.Fprintln(w, r.Form)
-	fmt.Fprintln(w,"\n#################################################################\n")
-	fmt.Fprintln(w,"                    CREATE SUBMISSION REPORT                      \n\n")
-	fmt.Fprintf(w,"Created Installation Env:")
-	fmt.Fprintln(w,r.Form["installname"])
-	fmt.Fprintln(w,"\n\n#################################################################")
+	fmt.Fprintln(w, "\n#################################################################\n")
+	fmt.Fprintln(w, "                    CREATE SUBMISSION REPORT                      \n\n")
+	fmt.Fprintf(w, "Created Installation Env:")
+	fmt.Fprintln(w, r.Form["installname"])
+	fmt.Fprintln(w, "\n\n#################################################################")
+
+	r.Form.Get("")
+
+	fmt.Printf(r.Form.Get("maclist"))
 }
